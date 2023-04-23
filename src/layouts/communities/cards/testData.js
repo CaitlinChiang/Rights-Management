0d5670da8b17e8f58935d0d8cba6ad458f132bb8
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2023-04-23 14:20:08 +0800

    Add more data

--- src/layouts/communities/cards/testData.js
+++ src/layouts/communities/cards/testData.js
@@ -46,5 +46,29 @@ export const testTribesData = [
     name: 'Community Name',
     location: 'Location Name',
     route: 'community/about'
+  },
+  {
+    imageUrl: 'https://www.coralia-liveaboard.com/wp-content/uploads/2020/05/Abui-tribe-dance-in-Alor-with-Coralia-Liveaboard-in-Indonesia.jpg',
+    name: 'Community Name',
+    location: 'Location Name',
+    route: 'community/about'
+  },
+  {
+    imageUrl: 'https://www.coralia-liveaboard.com/wp-content/uploads/2020/05/Abui-tribe-dance-in-Alor-with-Coralia-Liveaboard-in-Indonesia.jpg',
+    name: 'Community Name',
+    location: 'Location Name',
+    route: 'community/about'
+  },
+  {
+    imageUrl: 'https://www.coralia-liveaboard.com/wp-content/uploads/2020/05/Abui-tribe-dance-in-Alor-with-Coralia-Liveaboard-in-Indonesia.jpg',
+    name: 'Community Name',
+    location: 'Location Name',
+    route: 'community/about'
+  },
+  {
+    imageUrl: 'https://www.coralia-liveaboard.com/wp-content/uploads/2020/05/Abui-tribe-dance-in-Alor-with-Coralia-Liveaboard-in-Indonesia.jpg',
+    name: 'Community Name',
+    location: 'Location Name',
+    route: 'community/about'
   }
 ]
